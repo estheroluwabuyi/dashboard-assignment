f9e299c494a6884e92ed331511e52f29f32ca040
Esther Okiki <estherokiki15@gmail.com>
2026-01-09 19:11:30 +0100

    most ordered and your rating

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -19,17 +19,30 @@ function Sidebar() {
           <h2 className="text-[11px] tracking-[1px] leading-normal text-[#082431]/50 pl-10 py-3 pt-8">
             MENU
           </h2>
+          {/* Menu Items Loop */}
           {menu.map((item, index) => {
             const Icon = item.icon;
 
             return (
-              <div key={index} className="px-5 flex flex-col justify-center">
+              <div
+                key={index}
+                className="px-5 flex flex-col justify-center cursor-pointer group"
+              >
                 <div
-                  className={`font-medium flex items-center gap-3 w-50 pl-5 py-3 ${item.active ? "text-purple bg-[#707FDD]/10 rounded-[5px] " : "text-[#273240]"}`}
+                  className={`font-medium flex items-center gap-3 w-50 pl-5 py-3 transition-all duration-200
+          ${
+            item.active
+              ? "text-purple bg-[#707FDD]/10 rounded-[5px]"
+              : "text-[#273240] hover:text-purple hover:bg-[#707FDD]/10 hover:rounded-[5px]"
+          }`}
                 >
                   <Icon
                     size={18}
-                    className={`${item.active ? "" : "text-[#A6ABC8]"}`}
+                    className={`${
+                      item.active
+                        ? "text-purple"
+                        : "text-[#A6ABC8] group-hover:text-purple"
+                    }`}
                   />
                   <span className="leading-3">{item.title}</span>
                 </div>
@@ -46,13 +59,25 @@ function Sidebar() {
             const Icon = item.icon;
 
             return (
-              <div key={index} className="px-5 flex flex-col justify-center">
+              <div
+                key={index}
+                className="px-5 flex flex-col justify-center cursor-pointer group"
+              >
                 <div
-                  className={`font-medium flex items-center gap-3 w-50 pl-5 py-3 ${item.active ? "text-purple bg-[#707FDD]/10 rounded-[5px] " : "text-[#273240]"}`}
+                  className={`font-medium flex items-center gap-3 w-50 pl-5 py-3 transition-all duration-200
+          ${
+            item.active
+              ? "text-purple bg-[#707FDD]/10 rounded-[5px]"
+              : "text-[#273240] hover:text-purple hover:bg-[#707FDD]/10 hover:rounded-[5px]"
+          }`}
                 >
                   <Icon
                     size={18}
-                    className={`${item.active ? "" : "text-[#A6ABC8]"}`}
+                    className={`${
+                      item.active
+                        ? "text-purple"
+                        : "text-[#A6ABC8] group-hover:text-purple"
+                    }`}
                   />
                   <span className="leading-3">{item.title}</span>
                 </div>
