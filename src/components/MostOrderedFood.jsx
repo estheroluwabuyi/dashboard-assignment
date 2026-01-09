8810a26eb4cc2a0872b2a9ceb42ab7d64dc4d731
Esther Okiki <estherokiki15@gmail.com>
2026-01-09 19:07:56 +0100

    style most ordered food and dyour rating section

--- src/components/MostOrderedFood.jsx
+++ src/components/MostOrderedFood.jsx
@@ -14,22 +14,29 @@ function MostOrderedFood() {
       <p className="text-[13px] pt-3 leading-[21px] tracking-wide text-black/50">
         Adipiscing elit, sed do eiusmod tempor
       </p>
-      <div className="mt-10">
+
+      <div className="mt-10 flex flex-col gap-4">
         {foods.map((food, i) => (
           <div
             key={i}
-            className="flex items-center justify-between border-b border-gray-50 pb-5  "
+            className="flex items-center justify-between pb-5 border-b border-[#F8F9FB] last:border-0"
           >
-            <div className="flex items-center gap-5 h-[44px]">
-              <img
-                src={food.img}
-                alt={food.name}
-                className="rounded-full w-[28px] h-[28px]"
-              />
+
+       
+            <div className="flex items-center gap-3">
+              <div className="w-[28px] h-[28px] rounded-full overflow-hidden shadow-sm border border-[#DBE5EB]/80">
+                <img
+                  src={food.img}
+                  alt={food.name}
+                  className="w-full h-full object-cover"
+                />
+              </div>
+
               <p className="tracking-wide leading-[20px]  text-[#273240]">
                 {food.name}
               </p>
             </div>
+
             <p className=" text-[#273240]/70 tracking-wide leading-[20px] ">
               {food.price}
             </p>
